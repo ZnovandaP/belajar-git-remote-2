52339ca412c2d0b88800cf646010cd80a767fbf8
ZnovandaP <znovandaputra@gmail.com>
2023-03-24 11:45:50 +0700

    menambahaan logic closing bracket pada file script.js

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-console.log('hallo dunia')
-console.info('Ramadhan')
+const closingBracket = (str) => {
+  let open = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+
+  let close = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
+
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+    if (open[current]) {
+      stack.push(current);
+    } else if (close[current]) {
+      let last = stack.pop();
+      if (close[current] !== last) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
+
+console.log(closingBracket("((()))")); // true
+console.log(closingBracket("[[]](())")); // true
+console.log(closingBracket("((())")); // false
+console.log(closingBracket("][]")); // false
+console.log(closingBracket("({})")); // true
